a8ebf8306096fd66d353f68a3eefff97689926f3
mickey <mickeywu520@gmail.com>
2026-05-17 16:26:25 +0800

    Switch domain to gateway2go.bond

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.gateway2go.cc.cd/api/v1',
+  apiUrl: 'https://api.gateway2go.bond/api/v1',
   firebase: {
     apiKey: 'AIzaSyBWHj8Ia3kcJpzI-383guRYVEt4BlEWOfc',
-    authDomain: 'www.gateway2go.cc.cd',
+    authDomain: 'www.gateway2go.bond',
     projectId: 'cramschool-b4d52',
     storageBucket: 'cramschool-b4d52.firebasestorage.app',
     messagingSenderId: '81636913641',
